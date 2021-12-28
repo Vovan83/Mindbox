30f75e8cc30211566ed87bb36a62a5587ca456dd
Vovan83 <64544816+Vovan83@users.noreply.github.com>
2021-12-29 00:29:24 +0500

    done

--- Mindbox/Program.cs
+++ Mindbox/Program.cs
@@ -1,9 +1,9 @@
-﻿using System.Reflection;
+﻿using System.Collections.Generic;
 
 IShapeFactory shapeFactory = new SimpleShapeFactory();
 while (true)
 {
-    Console.WriteLine("Введите одно из допустимых названий фигуры ({0}). Для выхода введите пустую строку", string.Join(", ", shapeFactory.GetShapeNames()));
+    Console.WriteLine("Введите одно из допустимых названий фигуры ({0}).\r\nДля выхода введите пустую строку", string.Join(", ", shapeFactory.GetShapeNames()));
     var classShape = Console.ReadLine();
     if (string.IsNullOrEmpty(classShape))
         return;
@@ -16,6 +16,10 @@ while (true)
             shape.SetFromConsole(Console.In, Console.Out)
                  .ReportToConsole(Console.Out);
         }
+    }
+    catch (KeyNotFoundException)
+    {
+        Console.WriteLine($"{classShape} не найден");
     }
     catch (Exception ex)
     {
